e5dbcc6fc031881e47f55ba807461a620b2432c5
Bryant <fubuloubu@gmail.com>
2018-03-08 01:50:13 -0500

    Simplified contract a lot so it doesn't need to keep track of ownership or owner allowances

--- contracts/SampleICO.sol
+++ contracts/SampleICO.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.19;
 
-import "./ShillToken.sol";
+import "./ERC20Token.sol";
 import "./ICO.sol";
 
 contract SampleICO is ICO
@@ -8,27 +8,15 @@ contract SampleICO is ICO
     // ICO Information
     uint256 _tokenPrice = 1000; // wei per token
 
-    address public owner;
-
-    // Manage how many buys/sells can happen here
-    uint public OWNERSHIP_MAX;
-    mapping (address => uint) numPurchased;
-
     /* Token Contract pointer */
-    ShillToken token;
+    ERC20Token token;
 
     function SampleICO(address token_address)
         public
     {
-        // So we know who to transferFrom
-        owner = msg.sender;
-
         // Sets state variable 'token'
         // (which is an external contract interface)
-        token = ShillToken(token_address);
-
-        // Deterimine the maximum ownership limit to maintain fairness
-        OWNERSHIP_MAX = 10**uint256(token.decimals()); // (totalSupply / numParticipants)
+        token = ERC20Token(token_address);
 
         // emit event so that ICO_Watcher script knows what to look for
         RegisterICO(token_address); // Register with Token ICO watcher
@@ -43,10 +31,6 @@ contract SampleICO is ICO
         return _tokenPrice;
     }
 
-    // These functions are needed because Solidity doesn't have min/max (Copied from OpenZeppelin)
-    function max(uint a, uint b) internal pure returns (uint) { return a >= b ? a : b; }
-    function min(uint a, uint b) internal pure returns (uint) { return a <  b ? a : b; }
-
     /* buyTokens: buy floor(msg.value / tokenPrice()) Tokens */
     function buyTokens()
         public
@@ -56,18 +40,9 @@ contract SampleICO is ICO
         // (integer division floors automatically)
         uint256 tokensBought = (msg.value / tokenPrice());
 
-        // Ensure they aren't purchasing more than they can legally have
-        require(numPurchased[msg.sender] < OWNERSHIP_MAX);
-
-        // Limit them to that quantity (no underflow due to require ^)
-        tokensBought = min(OWNERSHIP_MAX-numPurchased[msg.sender], tokensBought);
-
-        // Keep track of how many they've bought
-        numPurchased[msg.sender] += tokensBought;
-
         // Be nice, give them the tokens
         // (could throw if allowance/balance doesn't allow)
-        token.transferFrom(owner, msg.sender, tokensBought);
+        token.transfer(msg.sender, tokensBought);
 
         // If you're extra nice, offer a refund of the remainder
         uint256 refund = msg.value - tokensBought * tokenPrice();
@@ -84,13 +59,8 @@ contract SampleICO is ICO
     function sellTokens(uint256 amount)
         public
     {
-        // Overflow protection
-        require(numPurchased[msg.sender] - amount >= 0);
-        // Keep track of how many they've sold back
-        numPurchased[msg.sender] -= amount;
-
         // Get your tokens first (throws if amount doesn't send)
-        assert(token.transferFrom(msg.sender, owner, amount));
+        assert(token.transferFrom(msg.sender, this, amount));
 
         // Then be nice and give them their money back
         uint256 refund = tokenPrice() * amount;
--- tests/test_ico.py
+++ tests/test_ico.py
@@ -1,37 +1,39 @@
 import pytest
 
-PARTICIPANTS=10
+
 @pytest.fixture
 def Token(tester):
-    return tester.contracts('contracts/ShillToken.sol').deploy(PARTICIPANTS)
+    return tester.contracts('contracts/ShillToken.sol').\
+            deploy(transact={'from': tester.accounts[1]})
+
 
 def test_token(tester, Token):
-    # Deployer address has all the tokens, and there is 1 token for each participant
-    assert Token.balanceOf(tester.accounts[0]) == \
-            Token.totalSupply() == \
-            PARTICIPANTS*10**Token.decimals()
+    # No one has any tokens
+    assert Token.totalSupply() == 0
+    # So mint some!
+    amount = 10**Token.decimals()
+    Token.mint(tester.accounts[1], amount)
+    assert Token.balanceOf(tester.accounts[1]) == amount
 
+
+PARTICIPANTS=10
 @pytest.fixture
 def ICO(tester, Token):
     return tester.contracts('contracts/SampleICO.sol').deploy(Token.address)
+
 
 def test_ico(tester, Token, ICO):
     # Test registration
     assert ICO.all_logs[-1]['token'] == Token.address
 
-    # Deployer has all the tokens
-    assert Token.balanceOf(tester.accounts[0]) == Token.totalSupply()
+    # ICO contract can only work if it has something to sell
+    Token.mint(ICO.address, PARTICIPANTS*10**Token.decimals())
     
-    # Allow the ICO to move tokens on your behalf
-    Token.approve(ICO.address, Token.totalSupply())
-    assert Token.allowance(tester.accounts[0], ICO.address) == Token.totalSupply()
-    
-    # Buy alloted tokens
-    amount = Token.totalSupply() // PARTICIPANTS
+    # Anyone can buy tokens
+    amount = Token.totalSupply()
     price = ICO.tokenPrice()
     starting_balance = tester.accounts[1].balance
-    # Even if we send way more than what we can purchase, we get that much
-    ICO.buyTokens(transact={'from': tester.accounts[1], 'value': 10 * amount * price})
+    ICO.buyTokens(transact={'from': tester.accounts[1], 'value': amount * price})
     assert Token.balanceOf(tester.accounts[1]) == amount
     assert tester.accounts[1].balance <= starting_balance - amount * price
     
