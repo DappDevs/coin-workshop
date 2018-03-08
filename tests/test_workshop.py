e07bcc8a680f92ed98e53ef344cb1bef8faa321e
Bryant <fubuloubu@gmail.com>
2018-03-08 16:17:55 -0500

    Got workshop working. Takes abut 25 mins to test

--- tests/test_workshop.py
+++ tests/test_workshop.py
@@ -20,6 +20,7 @@ def test_workshop(tester, Token, ICO_Factory):
     amt = 10**Token.decimals()
     [Token.mint(s, amt) for s in students]
     assert all([Token.balanceOf(s) == amt for s in students])
+    Token.mint(teacher, amt)  # Give self some tokens, for comparision
 
     # Students create ICOs
     ICOs = [ICO_Factory(s) for s in students]
@@ -39,11 +40,16 @@ def test_workshop(tester, Token, ICO_Factory):
     [Token.transfer(ICO.address, tkn_bal(s)//3, transact={'from': s}) \
             for ICO, s in zip(ICOs, students)]
     # As well as enough money to buy more
-    [s.send(ICO.address, tkn_prc(ICO)*tkn_bal(ICO.address)) \
+    [s.send(ICO.address, tkn_prc(ICO)*Token.totalSupply()) \
             for ICO, s in zip(ICOs, students)]
 
+    # Finally, students must authorize transfers to all ICOs
+    for s in students:
+        for ICO in ICOs:
+            Token.approve(ICO.address, Token.totalSupply(), transact={'from': s})
+
     # Students start trying to trade with other ICOs
-    for _ in range(30): # 30 minute excercise
+    for i in range(30): # 30 minute excercise
         buyer, seller = sample(students, 2)
         bICO, sICO = sample(ICOs, 2)
         try:
@@ -52,8 +58,14 @@ def test_workshop(tester, Token, ICO_Factory):
         except TransactionFailed as e:
             pass
 
-    # Students buyback all their tokens
-    [buy(ICO, s, tkn_bal(ICO.address)*tkn_prc(ICO)) for ICO, s in zip(ICOs, students)]
+        # Log who has what
+        print("Pass", i)
+        [print("Student", s, "has", tkn_bal(s), "tokens") for s in students]
+        [print("ICO owned by", ICO.owner(), "has", tkn_bal(ICO.address), "tokens") for ICO in ICOs]
+
+    # Students retreive all their tokens
+    [ICO.getTokens(transact={'from': s}) for ICO, s in zip(ICOs, students)]
+
     # Check for the winner
     winner = teacher
     for s in students:
