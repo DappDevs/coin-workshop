04ef20d14991d3e0505869d7ef0a3ee8a4e1897b
Bryant Eisenbach <fubuloubu@gmail.com>
2018-03-15 15:25:34 -0400

    Got Web3 loaded and working

--- src/App.js
+++ src/App.js
@@ -1,8 +1,46 @@
 import React, { Component } from 'react';
+import getWeb3 from './utils/getWeb3'
+
+import assetsFile from './contracts.json'
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      user: null,
+      web3: null,
+      contracts: null,
+      tokenInstance: null,
+      icoFactory: null,
+      icoInstances: [],
+    }
+  }
+
+  componentWillMount() {
+    // Get network provider and web3 instance.
+    // See utils/getWeb3 for more info.
+    getWeb3
+    .then(results => {
+      this.setState({
+        web3: results.web3,
+        user: results.web3.eth.coinbase
+      })
+      .then(
+        this.loadContractAssets()
+      )
+    })
+    .catch(() => {
+      console.log('Error finding web3.')
+    })
+  }
+
+  loadContractAssets() {
+    console.log(assetsFile.contracts)
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,7 +49,7 @@ class App extends Component {
           <h1 className="App-title">Coin Workshop</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Welcome <code>{this.state.user}</code>!
         </p>
       </div>
     );
